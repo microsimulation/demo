d2e36ecc5c5704aac46ff1a1ac39a5ebde4bd370
Dave Price <dave@daveprice.co.uk>
2019-06-11 10:12:31 +0100

    Add content and change navigation titles as per task #6

---
+++ journal/src/Controller/AboutPeopleControllerTest.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace test\eLife\Journal\Controller;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+final class AboutPeopleControllerTest extends PageTestCase
+{
+    /**
+     * @test
+     */
+    public function it_displays_the_people_page()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getUrl());
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame('Leadership team', $crawler->filter('.content-header__title')->text());
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_the_first_paragraph_of_aims_and_scope_for_a_subject()
+    {
+        $client = static::createClient();
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/subjects/subject',
+                ['Accept' => 'application/vnd.elife.subject+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.subject+json; version=1'],
+                json_encode([
+                    'id' => 'subject',
+                    'name' => 'Subject',
+                    'impactStatement' => 'Subject impact statement.',
+                    'aimsAndScope' => [
+                        [
+                            'type' => 'paragraph',
+                            'text' => 'Subject aims and scope.',
+                        ],
+                        [
+                            'type' => 'paragraph',
+                            'text' => 'Subject aims and scope more.',
+                        ],
+                    ],
+                    'image' => [
+                        'banner' => [
+                            'uri' => 'https://www.example.com/iiif/image',
+                            'alt' => '',
+                            'source' => [
+                                'mediaType' => 'image/jpeg',
+                                'uri' => 'https://www.example.com/image.jpg',
+                                'filename' => 'image.jpg',
+                            ],
+                            'size' => [
+                                'width' => 800,
+                                'height' => 600,
+                            ],
+                        ],
+                        'thumbnail' => [
+                            'uri' => 'https://www.example.com/iiif/image',
+                            'alt' => '',
+                            'source' => [
+                                'mediaType' => 'image/jpeg',
+                                'uri' => 'https://www.example.com/image.jpg',
+                                'filename' => 'image.jpg',
+                            ],
+                            'size' => [
+                                'width' => 800,
+                                'height' => 600,
+                            ],
+                        ],
+                    ],
+                ])
+            )
+        );
+
+        $this->mockPeopleApi([['leadership', 'senior-editor'], 'reviewing-editor'], 'subject');
+
+        $crawler = $client->request('GET', '/about/people/subject');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $this->assertSame('Subject aims and scope.', $crawler->filter('.content-header__impact-statement')->text());
+        $this->assertSame('Subject aims and scope.', $crawler->filter('meta[property="og:description"]')->attr('content'));
+        $this->assertSame('Subject aims and scope.', $crawler->filter('meta[name="description"]')->attr('content'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_default_impact_statement_if_subject_without_aims_and_scope()
+    {
+        $client = static::createClient();
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/subjects/subject',
+                ['Accept' => 'application/vnd.elife.subject+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.subject+json; version=1'],
+                json_encode([
+                    'id' => 'subject',
+                    'name' => 'Subject',
+                    'impactStatement' => 'Subject impact statement.',
+                    'image' => [
+                        'banner' => [
+                            'uri' => 'https://www.example.com/iiif/image',
+                            'alt' => '',
+                            'source' => [
+                                'mediaType' => 'image/jpeg',
+                                'uri' => 'https://www.example.com/image.jpg',
+                                'filename' => 'image.jpg',
+                            ],
+                            'size' => [
+                                'width' => 800,
+                                'height' => 600,
+                            ],
+                        ],
+                        'thumbnail' => [
+                            'uri' => 'https://www.example.com/iiif/image',
+                            'alt' => '',
+                            'source' => [
+                                'mediaType' => 'image/jpeg',
+                                'uri' => 'https://www.example.com/image.jpg',
+                                'filename' => 'image.jpg',
+                            ],
+                            'size' => [
+                                'width' => 800,
+                                'height' => 600,
+                            ],
+                        ],
+                    ],
+                ])
+            )
+        );
+
+        $this->mockPeopleApi([['leadership', 'senior-editor'], 'reviewing-editor'], 'subject');
+
+        $crawler = $client->request('GET', '/about/people/subject');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $this->assertSame('The working scientists who serve as eLife editors, our early-career advisors, governing board, and our executive staff all work in concert to realise eLife’s mission to accelerate discovery.', $crawler->filter('.content-header__impact-statement')->text());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_metadata()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getUrl().'?foo');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $this->assertSame('Leadership team | About | eLife', $crawler->filter('title')->text());
+        $this->assertSame('/about/people', $crawler->filter('link[rel="canonical"]')->attr('href'));
+        $this->assertSame('http://localhost/about/people', $crawler->filter('meta[property="og:url"]')->attr('content'));
+        $this->assertSame('Leadership team', $crawler->filter('meta[property="og:title"]')->attr('content'));
+        $this->assertSame('The working scientists who serve as eLife editors, our early-career advisors, governing board, and our executive staff all work in concert to realise eLife’s mission to accelerate discovery.', $crawler->filter('meta[property="og:description"]')->attr('content'));
+        $this->assertSame('The working scientists who serve as eLife editors, our early-career advisors, governing board, and our executive staff all work in concert to realise eLife’s mission to accelerate discovery.', $crawler->filter('meta[name="description"]')->attr('content'));
+        $this->assertSame('summary', $crawler->filter('meta[name="twitter:card"]')->attr('content'));
+        $this->assertEmpty($crawler->filter('meta[property="og:image"]'));
+        $this->assertEmpty($crawler->filter('meta[name="dc.identifier"]'));
+        $this->assertEmpty($crawler->filter('meta[name="dc.relation.ispartof"]'));
+        $this->assertEmpty($crawler->filter('meta[name="dc.title"]'));
+        $this->assertEmpty($crawler->filter('meta[name="dc.description"]'));
+        $this->assertEmpty($crawler->filter('meta[name="dc.date"]'));
+        $this->assertEmpty($crawler->filter('meta[name="dc.rights"]'));
+    }
+
+    protected function getUrl() : string
+    {
+        $this->mockPeopleApi(['leadership', 'senior-editor']);
+
+        return '/about/people';
+    }
+
+    protected function mockPeopleApi(array $types, $subject = null)
+    {
+        $subject_filter = ($subject) ? '&subject[]='.$subject : '';
+        foreach ($types as $type) {
+            $type = implode('', array_map(function (string $type) {
+                return "&type[]={$type}";
+            }, (array) $type));
+            $this->mockApiResponse(
+                new Request(
+                    'GET',
+                    'http://api.elifesciences.org/people?page=1&per-page=1&order=asc'.$subject_filter.$type,
+                    ['Accept' => 'application/vnd.elife.person-list+json; version=1']
+                ),
+                new Response(
+                    200,
+                    ['Content-Type' => 'application/vnd.elife.person-list+json; version=1'],
+                    json_encode([
+                        'total' => 0,
+                        'items' => [],
+                    ])
+                )
+            );
+        }
+    }
+}
---
+++ journal/src/Controller/HomeController.1.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace eLife\Journal\Controller;
+
+use eLife\ApiSdk\Collection\Sequence;
+use eLife\ApiSdk\Model\Subject;
+use eLife\Journal\Helper\Callback;
+use eLife\Journal\Helper\Paginator;
+use eLife\Journal\Pagerfanta\SequenceAdapter;
+use eLife\Patterns\ViewModel\Carousel;
+use eLife\Patterns\ViewModel\CarouselItem;
+use eLife\Patterns\ViewModel\LeadPara;
+use eLife\Patterns\ViewModel\LeadParas;
+use eLife\Patterns\ViewModel\Link;
+use eLife\Patterns\ViewModel\ListHeading;
+use eLife\Patterns\ViewModel\ListingTeasers;
+use eLife\Patterns\ViewModel\SectionListing;
+use eLife\Patterns\ViewModel\SectionListingLink;
+use eLife\Patterns\ViewModel\SeeMoreLink;
+use eLife\Patterns\ViewModel\Teaser;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use function GuzzleHttp\Promise\promise_for;
+
+final class HomeController extends Controller
+{
+    public function homeAction(Request $request) : Response
+    {
+        $page = (int) $request->query->get('page', 1);
+        $perPage = 10;
+
+        $arguments = $this->defaultPageArguments($request);
+
+        $latestResearch = promise_for($this->get('elife.api_sdk.search')
+            ->forType('research-advance', 'research-article', 'research-communication', 'scientific-correspondence', 'short-report', 'tools-resources', 'replication-study')
+            ->sortBy('date'))
+            ->then(function (Sequence $sequence) use ($page, $perPage) {
+                $pagerfanta = new Pagerfanta(new SequenceAdapter($sequence, $this->willConvertTo(Teaser::class)));
+                $pagerfanta->setMaxPerPage($perPage)->setCurrentPage($page);
+
+                return $pagerfanta;
+            });
+
+        $arguments['title'] = 'Latest research';
+
+        $arguments['paginator'] = $latestResearch
+            ->then(function (Pagerfanta $pagerfanta) use ($request) {
+                return new Paginator(
+                    'Browse our latest research',
+                    $pagerfanta,
+                    function (int $page = null) use ($request) {
+                        $routeParams = $request->attributes->get('_route_params');
+                        $routeParams['page'] = $page;
+
+                        return $this->get('router')->generate('home', $routeParams);
+                    }
+                );
+            });
+
+        $arguments['listing'] = $arguments['paginator']
+            ->then($this->willConvertTo(ListingTeasers::class, ['heading' => 'Latest research', 'type' => 'articles']));
+
+        if (1 === $page) {
+            return $this->createFirstPage($arguments);
+        }
+
+        return $this->createSubsequentPage($request, $arguments);
+    }
+
+    private function createFirstPage(array $arguments) : Response
+    {
+        $arguments['carousel'] = $this->get('elife.api_sdk.covers')
+            ->getCurrent()
+            ->map($this->willConvertTo(CarouselItem::class))
+            ->then(Callback::emptyOr(function (Sequence $covers) {
+                return new Carousel($covers->toArray(), new ListHeading('Highlights', 'highlights'));
+            }))
+            ->otherwise($this->softFailure('Failed to load covers'));
+
+        $arguments['leadParas'] = new LeadParas([new LeadPara('eLife works to improve research communication through open science and open technology innovation', 'strapline')]);
+
+        $arguments['subjectsLink'] = new SectionListingLink('All research categories', 'subjects');
+
+        $arguments['subjects'] = $this->get('elife.api_sdk.subjects')
+            ->reverse()
+            ->slice(1, 100)
+            ->map(function (Subject $subject) {
+                return new Link($subject->getName(), $this->get('router')->generate('subject', [$subject]));
+            })
+            ->then(function (Sequence $links) {
+                return new SectionListing('subjects', $links->toArray(), new ListHeading('Research categories'), false, 'strapline');
+            })
+            ->otherwise($this->softFailure('Failed to load subjects list'));
+
+        $arguments['announcements'] = $this->get('elife.api_sdk.highlights')
+            ->get('announcements')
+            ->slice(0, 3)
+            ->map($this->willConvertTo(Teaser::class, ['variant' => 'secondary']))
+            ->then(Callback::emptyOr(function (Sequence $highlights) {
+                return ListingTeasers::basic($highlights->toArray(), new ListHeading('New from eLife'));
+            }))
+            ->otherwise($this->softFailure('Failed to load announcements'));
+
+        $arguments['magazine'] = $this->get('elife.api_sdk.search')
+            ->forType('editorial', 'insight', 'feature', 'collection', 'interview', 'podcast-episode')
+            ->sortBy('date')
+            ->slice(1, 7)
+            ->then(Callback::emptyOr(function (Sequence $result) {
+                return ListingTeasers::withSeeMore(
+                    $result->map($this->willConvertTo(Teaser::class, ['variant' => 'secondary']))->toArray(),
+                    new SeeMoreLink(new Link('See more Magazine articles', $this->get('router')->generate('magazine'))),
+                    new ListHeading('Magazine')
+                );
+            }))
+            ->otherwise($this->softFailure('Failed to load Magazine list'));
+
+        return new Response($this->get('templating')->render('::home.html.twig', $arguments));
+    }
+}
